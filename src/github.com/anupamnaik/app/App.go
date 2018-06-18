6108f9e2daf4a6818f8bc84f4de744ac9434de0c
anupamnaik <naik.anupam@gmail.com>
2018-06-18 17:40:31 +0530

    New swap functions.

--- src/github.com/anupamnaik/app/App.go
+++ src/github.com/anupamnaik/app/App.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"github.com/anupamnaik/app/functions"
 	"fmt"
+
+	"github.com/anupamnaik/app/functions"
 )
+
+func swap(x, y int) (int, int) {
+	return y, x
+}
 
 func main() {
 	fmt.Println("Strating application...")
 	fmt.Println(functions.RandomNumberInt(100))
+
+	a, b := functions.SwapInt(12, 34)
+	fmt.Println(a, b)
 
 	fmt.Println("End here.")
 }
---
+++ src/github.com/anupamnaik/app/functions/Swapper.go
@@ -0,0 +1,11 @@
+package functions
+
+// SwapInt swaps two integers.
+func SwapInt(x, y int) (int, int) {
+	return y, x
+}
+
+// SwapString swaps tow strings.
+func SwapString(x, y string) (string, string) {
+	return y, x
+}
